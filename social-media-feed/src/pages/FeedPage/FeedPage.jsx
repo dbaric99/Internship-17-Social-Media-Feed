ce6156417bf97f1072af88fb3d0cfcf412bcc445
dbaric99 <damjana.baric1999@gmail.com>
2023-03-28 22:42:41 +0200

    Added separate component for displaying posts

--- social-media-feed/src/pages/FeedPage/FeedPage.jsx
+++ social-media-feed/src/pages/FeedPage/FeedPage.jsx
@@ -1,9 +1,7 @@
-import styles from './FeedPage.module.css';
 import posts from '../../data/posts.json';
 import { FeedItem } from '.';
 
 function FeedPage() {
-  
   return (
     <div>
       {posts.map(post => <FeedItem key={post.id} post={post} />)}
--- social-media-feed/src/pages/PostPage/PostPage.jsx
+++ social-media-feed/src/pages/PostPage/PostPage.jsx
@@ -1,8 +1,20 @@
+import { useNavigate, useParams } from 'react-router-dom';
 import styles from './PostPage.module.css';
+import posts from '../../data/posts.json';
+import { useEffect, useState } from 'react';
+import { CardItem } from 'components/CardItem';
 
 function PostPage() {
+  const postId = useParams().postId;
+  const navigate = useNavigate();
+  const [postObject, setPostObject] = useState(posts.filter(post => post.id === +postId)[0]);
+
+  //add check if postId exists
+
   return (
-    <div>PostPage</div>
+    <CardItem post={postObject}>
+
+    </CardItem>
   )
 }
 
